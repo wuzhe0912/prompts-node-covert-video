05676410a785d5c7d468f37202441439ce9c6161
Pitt Wu <pitt.wu@hlwtech.com.tw>
2022-06-13 14:05:43 +0800

    feat: update mock data

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../assets/css/style.css';
+
+const initialPosts = [
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar1.png',
+      username: 'Test User',
+    },
+  },
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar2.png',
+      username: 'Test User 2',
+    },
+  },
+];
 
 function App() {
+  console.log(1, initialPosts);
+  const [posts, setPosts] = useState(initialPosts);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="feed">
+        {initialPosts.map((post) => {
+          return (
+            <div className="post" key={post.id}>
+              <div className="header">
+                <img src={post.user.avatar} alt={post.user.username} />
+                <h2>{post.user.username}</h2>
+              </div>
+              <p>{post.text}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
